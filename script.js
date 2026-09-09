a1bcc8578ae20b33d6282609f7088e2503519ef4
thepiyushkushwaha <piyushkushwaha.medium@gmail.com>
2026-09-10 01:24:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function patientLogin() {
+    alert("Patient section opened");
+}
+
+function doctorLogin() {
+    alert("Doctor section opened");
+}
+
+function ayushLogin() {
+    alert("AYUSH Doctor section opened");
+}
